16e2c659fbe862a056dca16d45f229c18292b26a
Sadegh hosseiney <sadeghmessi2015@gmail.com>
2020-01-25 11:17:08 -0500

    define GeneralDefines for more beauty

---
+++ clash of basu/include/GeneralDefines.h
@@ -0,0 +1,32 @@
+#ifndef GENERALDEFINES_H
+#define GENERALDEFINES_H
+
+#include "UI_Menu.h"
+#include "Hero.h"
+#include "HeroCards.h"
+#include "Field.h"
+#include "AlphaManCard.h"
+#include "CommanderCard.h"
+#include "DrMarryCard.h"
+#include "GiantCrad.h"
+#include "KratosCard.h"
+#include "LeonCard.h"
+#include "MrsGhostCard.h"
+#include "ProfessorCard.h"
+#include "RobiCard.h"
+#include "SniperCard.h"
+
+enum class menus{SM = 0, MM};
+
+class GeneralDefines
+{
+    public:
+        GeneralDefines();
+        ~GeneralDefines();
+
+    protected:
+
+    private:
+};
+
+#endif // GENERALDEFINES_H
